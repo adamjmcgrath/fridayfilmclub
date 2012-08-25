f42a45394c79958247268e2b9af546f46b5206be
Adam Mcgrath <adamjmcgrath@gmail.com>
2012-08-25 15:21:45 +0100

    temp

--- fabfile.py
+++ fabfile.py
@@ -197,7 +197,7 @@ def get_tags_name():
     num = 1
   else:
     print last_tag_name
-    match = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}-([0-9]*)',
+    match = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}\.([0-9]*)',
         last_tag_name)
     num = int(match.group(1)) + 1
 
