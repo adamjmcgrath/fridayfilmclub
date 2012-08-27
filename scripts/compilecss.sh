fc6c037893a705b8050a08d0efbc544a9760ce1d
Adam Mcgrath <adamjmcgrath@gmail.com>
2012-08-27 23:03:56 +0100

    Add compile css script

---
+++ scripts/compilecss.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Copyright 2011 Friday Film Club Inc.  All Rights Reserved.
+#
+# Tools for compiling CSS using Less compiler.
+
+lessc --compress src/stylesheets/main.less > src/static/css/main.css
