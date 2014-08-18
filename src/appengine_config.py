350e04bc4197da0eb1ae1a1df1df16458d5deafe
adammcgrath <adam@thisisglow.com>
2014-08-18 21:30:35 +0100

    more requirements changes

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -9,7 +9,7 @@ __author__ = 'adamjmcgrath@gmail.com (Adam McGrath)'
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'gaenv_lib'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
 
 # Enabled copying data from the movieautocomplete app.
 remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
